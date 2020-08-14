5c54d596b6735e05c13d31b2548c777bf6dac6b9
rfloweree <rubenloweree@gmail.com>
2020-08-14 09:35:50 -0600

    Removed the process method.

--- src/main/java/com/rfl/sweng861/searchspotify/servlet/SpotifyServletController.java
+++ src/main/java/com/rfl/sweng861/searchspotify/servlet/SpotifyServletController.java
@@ -1,7 +1,6 @@
 package com.rfl.sweng861.searchspotify.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,32 +19,6 @@ import javax.servlet.http.HttpServletResponse;
             "/songs"
         })
 public class SpotifyServletController extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet SpotifyServletController</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet SpotifyServletController at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
     /**
      * Handles the HTTP <code>GET</code> method.
      *
